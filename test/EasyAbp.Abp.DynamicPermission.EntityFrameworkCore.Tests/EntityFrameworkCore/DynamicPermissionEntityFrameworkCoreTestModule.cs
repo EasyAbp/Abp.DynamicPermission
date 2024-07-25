222b34db7e3059c81e51c3b313fb0405397fa361
gdlcf88 <gdlcf88@outlook.com>
2024-07-25 18:36:11 +0800

    Upgrade to ABP 8.2.0

--- test/EasyAbp.Abp.DynamicPermission.EntityFrameworkCore.Tests/EntityFrameworkCore/DynamicPermissionEntityFrameworkCoreTestModule.cs
+++ test/EasyAbp.Abp.DynamicPermission.EntityFrameworkCore.Tests/EntityFrameworkCore/DynamicPermissionEntityFrameworkCoreTestModule.cs
@@ -34,7 +34,7 @@ namespace EasyAbp.Abp.DynamicPermission.EntityFrameworkCore
         
         private static SqliteConnection CreateDatabaseAndGetConnection()
         {
-            var connection = new SqliteConnection("Data Source=:memory:");
+            var connection = new AbpUnitTestSqliteConnection("Data Source=:memory:");
             connection.Open();
 
             new DynamicPermissionTestDbContext(
